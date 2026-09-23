c69d09d4a49fe7c76e1609bc2df89d4e85d98a51
wenshao <szujobs@gmail.com>
2026-09-24 02:15:31 +0800

    PR 12182 verification screenshots

--- docs/users/ide-integration/_meta.ts
+++
@@ -1,4 +0,0 @@
-export default {
-  'ide-integration': 'Introduction',
-  'ide-companion-spec': 'IDE Companion Spec',
-};
--- packages/sdk-java/client/src/main/java/com/alibaba/acp/sdk/protocol/jsonrpc/ExtNotification.java
+++
@@ -1,4 +0,0 @@
-package com.alibaba.acp.sdk.protocol.jsonrpc;
-
-public class ExtNotification extends MethodMessage<Object> {
-}
